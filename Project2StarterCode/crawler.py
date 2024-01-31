74c649adda1b2e148c8e1ad843a0308862613e79
nksongg <nksongg@gmail.com>
2024-01-30 19:02:24 -0800

    implemented crawler traps

--- Project2StarterCode/crawler.py
+++ Project2StarterCode/crawler.py
@@ -12,7 +12,7 @@ class Crawler:
     the frontier
     """
     STOP_WORDS = {"a","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
-
+    
     def __init__(self, frontier, corpus):
         self.frontier = frontier
         self.corpus = corpus
@@ -44,7 +44,7 @@ class Crawler:
             url_data = self.corpus.fetch_url(url)
 
             for next_link in self.extract_next_links(url_data):
-                if self.is_valid(next_link):
+                if self.is_valid(next_link,url_data["content"]):
                     if self.corpus.get_file_name(next_link) is not None:
                         self.frontier.add_url(next_link)
 
@@ -118,6 +118,8 @@ class Crawler:
                 htmlFile.make_links_absolute(url_data["url"])
                 
                 #update word counts excluding html markup
+                #still need to update the stop words being filtered out
+                
                 text = htmlFile.text_content()
                 token_list = self.word_token_count(text)
                 for token in token_list:
@@ -159,7 +161,7 @@ class Crawler:
         return outputLinks
 
 
-    def is_valid(self, url):
+    def is_valid(self, url,content=None):
         """
         Function returns True or False based on whether the url has to be fetched or not. This is a great place to
         filter out crawler traps. Duplicated urls will be taken care of by frontier. You don't need to check for duplication
@@ -181,6 +183,16 @@ class Crawler:
         #check for recursion by checking for queries
         if len(parse_qs(parsed.query)) > max_query_parameters:
             return False
+        
+        #check if the lengths of the pages are the same so we can avoid traps where its the same page
+        if content:
+            content_length = len(content)
+            
+            if content_length in self.content_lengths:
+                return False
+            else:
+                self.content_lengths.add(content_length)
+            
         
         if parsed.scheme not in set(["http", "https"]):
             return False
