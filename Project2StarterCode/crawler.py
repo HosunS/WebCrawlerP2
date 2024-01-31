064f1fd9fef5f6a07eb027af14e4dd61db0decd6
Rebecca Park <sbvts177@gmail.com>
2024-01-30 18:41:50 -0800

    fixed word count to remove stop words

--- Project2StarterCode/crawler.py
+++ Project2StarterCode/crawler.py
@@ -11,6 +11,7 @@ class Crawler:
     This class is responsible for scraping urls from the next available link in frontier and adding the scraped links to
     the frontier
     """
+    STOP_WORDS = {"a","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
 
     def __init__(self, frontier, corpus):
         self.frontier = frontier
@@ -114,12 +115,11 @@ class Crawler:
                 htmlFile.make_links_absolute(url_data["url"])
                 
                 #update word counts excluding html markup
-                #still need to update the stop words being filtered out
-                
                 text = htmlFile.text_content()
                 token_list = self.word_token_count(text)
                 for token in token_list:
-                    self.word_count[token] = self.word_count.get(token,0) + 1
+                    if token not in self.STOP_WORDS:
+                        self.word_count[token] = self.word_count.get(token,0) + 1
 
 
                 #text = etree.tostring(htmlFile,method='text',encoding='utf-8').decode('utf-8')
